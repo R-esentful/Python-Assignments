acb3056ad6421f6cea6d7160baf52f56f3056988
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2021-06-22 13:02:18 +0800

    Update SB7.py
    Optimized run time

--- SB7.py
+++ SB7.py
@@ -1,22 +1,17 @@
 def List():
     words = []
-    counter = False
     word_checker = 0
-    while counter == False:
-        user_input = input("Enter a Word:")
-        words.append(user_input)
-        word_checker += 1
-        choice = input("Try again? Y/y or N/n:")
-        user_choice = choice.casefold()
-        if user_choice == 'y':
-            counter = False
-        elif user_choice == 'n':
-            counter= True
-            print("Inputted Words ",words)
-            print("Total number of words:{}".format(word_checker))
-        else:
-            print("Invalid choice!")
-
+    choice = 'y'
+    while choice == 'y' or choice == 'Y':
+        word_input = []
+        word = input("Enter a word:")
+        word_input.append(word)
+        word_checker +=1
+        for x in word_input:
+            words.append(x)
+        choice = input("Try again? [Y/y] or [N/n]:")
+    print("Inputted words:{}".format(words))
+    print("Total number of words:{}".format(word_checker))
 
 
 List()
